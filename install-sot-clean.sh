fc1b35021106bba50575a3819d995eeb32e386fb
acmeproducts <182510693+acmeproducts@users.noreply.github.com>
2026-08-23 05:31:05 -0700

    Pin clean SOT installer to immutable rebuild

--- install-sot-clean.sh
+++ install-sot-clean.sh
@@ -5,7 +5,7 @@ REPORT_ROOT=/home/support/.openclaw/workspace/https/report
 SOT_STATE=/home/support/.openclaw/sot
 SOT_DATABASE="$SOT_STATE/sot.sqlite"
 SERVICE=openclaw-report-server.service
-BASE=https://raw.githubusercontent.com/acmeproducts/stuff/__PINNED_COMMIT__
+BASE=https://raw.githubusercontent.com/acmeproducts/stuff/6f5e76c2095fa1a80476685e08c3ace55b68ea42
 PUBLIC_URL='https://oc-ref.fell-dojo.ts.net/report/SOT/sot-turn01-r2-wizard.html?v=20260823-clean1'
 TEMP_ROOT="$(mktemp -d)"
 trap 'rm -rf "$TEMP_ROOT"' EXIT
